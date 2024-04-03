4b5f53fb9be2fc6c4fbece82acccacdaf77ef70a
pxuria <pouriamesram@gmail.com>
2024-04-03 01:19:28 -0700

    optimization v2

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const userRouter = require("./Routes/userRoutes");
 const app = express();
 
 //Middlewares
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
