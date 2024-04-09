247cfd528748cdf606cc99be5b974d52f1ffc9b5
pxuria <pouriamesram@gmail.com>
2024-04-09 14:42:55 -0700

    setting up mongodb driver

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => console.log("DB Conneted"));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
