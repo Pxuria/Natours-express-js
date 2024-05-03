ae7ace9eb9c189398b8c2b70a74c6147b865aae5
pxuria <pouriamesram@gmail.com>
2024-05-03 15:20:37 -0700

    data validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name."],
       trim: true,
       unique: true,
+      maxlength: [40, "A tour name must have less or equal than 40 characters"],
+      minlength: [10, "A tour name must have more or equal than 10 characters"],
     },
     slug: String,
     duration: {
@@ -21,10 +23,13 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: { values: ["easy", "medium", "difficult"], message: "Difficulty is either easy,medium or difficult" },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Ratings must be above 1.0"],
+      max: [5, "Ratings must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
