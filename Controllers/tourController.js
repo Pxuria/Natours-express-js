5da33c9439cbe73e7b37382f318c282e7299ac2b
pxuria <pouriamesram@gmail.com>
2024-05-07 00:14:43 -0700

    creating Errors During Development vs Production

--- Controllers/tourController.js
+++ Controllers/tourController.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const Tour = require("../models/tourModel");
 const APIFeatures = require("../utils/apiFeatures.js");
 const catchAsync = require("../utils/catchAsync.js");
+const AppError = require("../utils/appError.js");
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, "utf-8"));
 
@@ -34,6 +35,8 @@ exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // const tour = await Tour.findOne({ _id: req.params.id });
 
+  if (!tour) return next(new AppError("No tour found with that ID", 404));
+
   res.status(200).json({
     status: "success",
     data: { tour },
@@ -56,6 +59,8 @@ exports.createTour = catchAsync(async (req, res, next) => {
 exports.updateTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
+  if (!tour) return next(new AppError("No tour found with that ID", 404));
+
   res.status(200).json({
     status: "success",
     data: { tour },
@@ -64,6 +69,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) return next(new AppError("No tour found with that ID", 404));
+
   res.status(204).json({
     status: "success",
     data: null,
